21944b928e840d4fe78b5135db49daf602a98043
Arthur Pereira <arthur.t.pereira@edu.ufes.br>
2023-10-22 16:07:14 -0300

    Comentários

--- horta/horta.ino
+++ horta/horta.ino
@@ -1,43 +1,49 @@
 #include <Wire.h>
 #include <ThingerESP8266.h>
 
+// Conexão Wifi
 const char* ssid = "CLARO AP 207";
 const char* password = "33017490";
 
+// Conexão Thinger.io
 const char* username = "arthur_tpereira";
 const char* deviceId = "esp8266";
 const char* deviceCredential = "123456";
 
+// Dados dos sensores
 float umidadeSolo = 0;
 float temperatura = 0;
 float umidadeAr = 0;
 float luminosidade = 0;
 
-
+// Instancia do Thinger.io
 ThingerESP8266 thing(username,deviceId,deviceCredential);
 
+// Setup
+// Configura o I2C e as saídas de dados para o Thinger.io
 void setup() {
     Wire.begin(0,2);
     Serial.begin(9600);
     delay(100);
-    Serial.print("Master Ready");
     thing.add_wifi(ssid,password);
 
     thing["umidade_solo"] >> outputValue(umidadeSolo);
     thing["temperatura"] >> outputValue(temperatura);
     thing["umidade_ar"] >> outputValue(umidadeAr);
     thing["luminosidade"] >> outputValue(luminosidade);
+    
+    Serial.print("Master configurado");
 }
 
 void loop() {
 
     String data = "";
 
-    Wire.requestFrom(8,10);         // Request umidade do solo
-
+    // Request umidade do solo
+    Wire.requestFrom(8,10);
     while (Wire.available()) {
         char c = Wire.read();
-        if (c == 'X') break;
+        if (c == 'X') break;        // 'X' == Fim de string
         data+=c;
     }
     data.trim();
@@ -45,11 +51,12 @@ void loop() {
 
     data = "";
     delay(100);
-    Wire.requestFrom(8,10);         // Request temperatura
 
+    // Request temperatura
+    Wire.requestFrom(8,10);
     while (Wire.available()) {
         char c = Wire.read();
-        if (c == 'X') break;
+        if (c == 'X') break;        // 'X' == Fim de string
         data+=c;
     }
     data.trim();
@@ -57,11 +64,12 @@ void loop() {
 
     data = "";
     delay(100);
-    Wire.requestFrom(8,10);         // Request umidade do ar
 
+    // Request umidade do ar
+    Wire.requestFrom(8,10);
     while (Wire.available()) {
         char c = Wire.read();
-        if (c == 'X') break;
+        if (c == 'X') break;        // 'X' == Fim de string
         data+=c;
     }
     data.trim();
@@ -69,15 +77,17 @@ void loop() {
 
     data = "";
     delay(100);
-    Wire.requestFrom(8,10);         // Request luminosidade
 
+    // Request luminosidade
+    Wire.requestFrom(8,10);
     while (Wire.available()) {
         char c = Wire.read();
-        if (c == 'X') break;
+        if (c == 'X') break;        // 'X' == Fim de string
         data+=c;
     }
     data.trim();
     luminosidade = data.toFloat();
+
 
     thing.handle();
     delay(2000);
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,29 +1,36 @@
 #include <Wire.h>
 #include <DHT.h>
 
+// Pinos para os sensores
 #define UMIDADE A0
 #define DHTPIN A1
 #define DHTTYPE DHT11
 
+// Instancia do DHT
 DHT dht(DHTPIN,DHTTYPE);
 
+// Sensor atual
 int state = 0;
 
+// Dados dos sensores
 float umidadeSolo = 0;
 float temperatura = 0;
 float umidadeAr = 0;
 float luminosidade = 0;
 
+// Setup
+// Configura o DHT, os pinos dos sensores e o I2C
 void setup() {
     dht.begin();
-    pinMode(UMIDADE,INPUT);            // leitura sensor umidade
+    pinMode(UMIDADE,INPUT);           
     Serial.begin(9600);
     Wire.begin(8);                // join i2c bus with address #8
     Wire.onRequest(requestEvent); // register event
-    Serial.print("Slave Ready");
-
+    Serial.println("Slave configurado");
 }
 
+// Loop
+// Leitura do sensor de umidade e temperatura
 void loop() {
 
     umidadeAr = dht.readHumidity();
@@ -37,33 +44,33 @@ void loop() {
   delay(100);
 }
 
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
+// Função executada ao receber um request pelo master
 void requestEvent() {
     Serial.println("Request recebido");
 
     String str;
     float data;
 
+    // Reset do numero do sensor
     if (state == 4) {
         state = 0;
     }
     
-    if (state == 0) {               // Sensor de umidade do solo
+    if (state == 0) {                       // Sensor de umidade do solo
         state++;
         int umidadeAnalog = analogRead(UMIDADE);
         data = (100 - ((umidadeAnalog/1023.0) * 100));
-        str = String(data) + 'X';   // 'X' = Fim de string
+        str = String(data) + 'X';           // 'X' = Fim de string
         Wire.write(str.c_str());    
-    } else if (state == 1) {        // Sensor de temperatura
+    } else if (state == 1) {                // Sensor de temperatura
         state++;
-        str = String(temperatura) + 'X';
+        str = String(temperatura) + 'X';    // 'X' = Fim de string
         Wire.write(str.c_str());
-    } else if (state == 2) {
+    } else if (state == 2) {                // Sensor de umidade do ar
         state++;
-        str = String(umidadeAr) + 'X';
+        str = String(umidadeAr) + 'X';      // 'X' = Fim de string
         Wire.write(str.c_str());
-    } else if (state == 3) {
+    } else if (state == 3) {                // Sensor de luminosidade
         state++;
         str = "3X";
         Wire.write(str.c_str());
